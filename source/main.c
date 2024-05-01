1fd3409e82bdb69a1aca96fd3d0c96a5bfd7e7f0
Jan Hofmeier <jan.hofmeier@gmail.com>
2024-05-02 01:26:30 +0200

    make the format work

--- source/main.c
+++ source/main.c
@@ -14,15 +14,36 @@
 #define HEAP_ID 0xCAFF
 #define DEVTYPE_USB 17
 
-#define LD_DWORD(ptr)       (u16)(((u32)*((u8*)(ptr)+3)<<24)|((u32)*((u8*)(ptr)+2)<<16)|((u16)*((u8*)(ptr)+1)<<8)|*(u8*)(ptr))
+#define SERVER_HANDLE_LEN 0xb5
+#define SERVER_HANDLE_SZ (SERVER_HANDLE_LEN * sizeof(int))
+
+#define LD_DWORD(ptr)       (u32)(((u32)*((u8*)(ptr)+3)<<24)|((u32)*((u8*)(ptr)+2)<<16)|((u16)*((u8*)(ptr)+1)<<8)|*(u8*)(ptr))
 
 static int (*FSSAL_attach_device)(int*) = (void*)0x10733aa4;
 
-static int sdusb_attach_device_handle[0xb5];
+#define FIRST_HANDLE ((int*)0x11c39e78)
+#define HANDLE_END ((int*)0x11c3a420)
+
+int extra_device_handle[SERVER_HANDLE_LEN]; // = HANDLE_END-SERVER_HANDLE_LEN;
 
 static u32 sdusb_offset = 0xFFF;
 static int (*real_read)(int*, u32, u32, u32, u32, void*, void*, void*) = (void*)0x107bddd0;
 static int (*real_write)(int*, u32, u32, u32, u32, void*, void*, void*) = (void*)0x107bdd60;
+
+
+int print_handles(){
+    for(int* handle = FIRST_HANDLE; handle<HANDLE_END; handle+=SERVER_HANDLE_LEN){
+        debug_printf("SDUSB: Server handle %p:\n", handle);
+        if(!*handle){
+            debug_printf("(unused)\n");
+            continue;
+        }
+        for(int i=0; i<SERVER_HANDLE_LEN; i++){
+            debug_printf("%02X: %08X\n", i, handle[i]);
+        }
+    }
+}
+
 
 static int read_wrapper(void *device_handle, u32 lba_hi, u32 lba, u32 blkCount, u32 blockSize, void *buf, void *cb, void* cb_ctx){
     return real_read(device_handle, lba_hi, lba + sdusb_offset, blkCount, blockSize, buf, cb, cb_ctx);
@@ -53,11 +74,11 @@ static void read_callback(int res, cb_ctx *ctx){
 }
 
 void hook_register_sd(trampoline_state *state){
-    memcpy(sdusb_attach_device_handle, (int*) state->r[6] -3, sizeof(sdusb_attach_device_handle));
-    int *device_handle = (int*)state->r[0] -3;
-    real_read = (void*)device_handle[0x76];
-    real_write = (void*)device_handle[0x78];
-    u8 *buf = iosAllocAligned(0xCAFF, SECTOR_SIZE, 0x40);
+    int *server_handle = (int*)state->r[0] -3;
+    debug_printf("SDUSB: org server_handle: %p\n", server_handle);
+    real_read = (void*)server_handle[0x76];
+    real_write = (void*)server_handle[0x78];
+    u8 *buf = iosAllocAligned(HEAP_ID, SECTOR_SIZE, 0x40);
     if(!buf){
         debug_printf("SDUSB: Failed to allocate IO buf\n");
         return;
@@ -69,7 +90,7 @@ void hook_register_sd(trampoline_state *state){
     }
 
     debug_printf("Calling sdio_read at %p\n", real_read);
-    int res = real_read(device_handle, 0, 0, 1, SECTOR_SIZE, buf, read_callback, &ctx);
+    int res = real_read(server_handle, 0, 0, 1, SECTOR_SIZE, buf, read_callback, &ctx);
     debug_printf("sdio_read returned: %u\n", res);
 
     debug_printf("SDUSB: Waiting for semaphore\n");
@@ -89,26 +110,50 @@ void hook_register_sd(trampoline_state *state){
 
     if(!part){
         debug_printf("SDUSB: USB partition not found!!!\n");
+        iosFree(HEAP_ID, buf); // also frees part
         return;
     }
     
     sdusb_offset = LD_DWORD(part->lba_start);
     u32 size = LD_DWORD(part->lba_length);
 
+    debug_printf("SDUSB: raw part offset: %02X %02X %02X %02X, size: %02X %02X %02X %02X\n", 
+            part->lba_start[0], part->lba_start[1], part->lba_start[2], part->lba_start[3],
+                part->lba_length[0], part->lba_length[1],part->lba_length[2],part->lba_length[3]);
+
     iosFree(HEAP_ID, buf); // also frees part
 
-    debug_printf("SDUSB: USB partition found: offset: %u, size: %u\n", sdusb_offset, size);
+    debug_printf("SDUSB: USB partition found %p: offset: %u, size: %u\n", part, sdusb_offset, size);
 
-    sdusb_attach_device_handle[0x3] = (int) sdusb_attach_device_handle;
-    sdusb_attach_device_handle[0x76] = (int)read_wrapper;
-    sdusb_attach_device_handle[0x76] = (int)write_wrapper;
-    sdusb_attach_device_handle[0x5] = DEVTYPE_USB;
-    sdusb_attach_device_handle[0xa] = size -1;
-    sdusb_attach_device_handle[0xa] = size;
+    //print_handles();
 
-    res = FSSAL_attach_device(sdusb_attach_device_handle+3);
+    memcpy(extra_device_handle, server_handle, SERVER_HANDLE_SZ);
+    res = FSSAL_attach_device(extra_device_handle+3);
+    extra_device_handle[0x82] = res;
+
+    int *sdusb_server_handle = server_handle;
+
+    sdusb_server_handle[0x3] = (int) sdusb_server_handle;
+    sdusb_server_handle[0x76] = (int)read_wrapper;
+    sdusb_server_handle[0x78] = (int)write_wrapper;
+    sdusb_server_handle[0x5] = DEVTYPE_USB;
+    sdusb_server_handle[0xa] = size -1;
+    sdusb_server_handle[0x1] =sdusb_server_handle[0xe] = size;
+
+    //sdusb_attach_device_handle[0x83] = 0xFF;
+
+    //res = FSSAL_attach_device(sdusb_attach_device_handle+3);
+    //sdusb_attach_device_handle[0x82] = res;
 
     debug_printf("SDUSB: Attached pseudo USB device. res: 0x%X\n", res);
+}
+
+int replace_register_sd(int *attach_arg, int r1, int r2, int r3, int(*sal_attach)(int*)){
+    int *last_server_hanlde = (int*)0x11c3a420-SERVER_HANDLE_LEN;
+    memcpy(last_server_hanlde, attach_arg -3, SERVER_HANDLE_SZ);
+    last_server_hanlde[0x3] = (int) last_server_hanlde;
+    last_server_hanlde[0x82] = sal_attach(last_server_hanlde +3);
+    return sal_attach(attach_arg);
 }
 
 
@@ -124,7 +169,11 @@ void kern_main()
 
     debug_printf("init_linking symbol at: %08x\n", wafel_find_symbol("init_linking"));
 
+    //ASM_PATCH_K(0x107bd98c, "cmp r3,#17");
+    //ASM_PATCH_K(0x107BD87C, "mov r3,#17");
+    //ASM_PATCH_K(0x107bd9a4, "nop");
     trampoline_hook_before(0x107bd9a4, hook_register_sd);
+    //trampoline_blreplace(0x107bd9a4, replace_register_sd);
 
 }
 
