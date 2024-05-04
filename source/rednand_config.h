6bd2dd5b1c0ceb5b9cdf776247681e93dd8cb52c
Jan Hofmeier <jan.hofmeier@gmail.com>
2024-05-05 01:00:04 +0200

    rednand_config.h

---
+++ source/rednand_config.h
@@ -0,0 +1,33 @@
+#include <wafel/types.h>
+
+#ifndef REDNAND_CONFIG_H
+#define REDNAND_CONFIG_H
+
+typedef struct {
+    u32 lba_start;
+    u32 lba_length;
+} PACKED rednand_partition;
+
+typedef struct {
+    rednand_partition slc;
+    rednand_partition slccmpt;
+    rednand_partition mlc;
+    bool disable_scfm;
+    bool scfm_on_slccmpt;
+    bool initilized;
+} PACKED rednand_config_v1;
+
+typedef struct {
+    rednand_partition slc;
+    rednand_partition slccmpt;
+    rednand_partition mlc;
+    bool disable_scfm;
+    bool scfm_on_slccmpt;
+    bool initilized;
+    //v2
+    bool slc_nocrypto; //not implemented yet
+    bool slccmpt_nocrypto; //not implemented yet
+    bool mlc_nocrypto;
+} PACKED rednand_config;
+
+#endif
