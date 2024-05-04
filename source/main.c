67511df5f3009ebea0c0123aa3a06ee1d65b49fb
Jan Hofmeier <jan.hofmeier@gmail.com>
2024-05-05 00:34:06 +0200

    redirect HAI

--- source/main.c
+++ source/main.c
@@ -8,7 +8,10 @@
 #include <wafel/patch.h>
 #include <wafel/ios/svc.h>
 #include <wafel/trampoline.h>
+#include "wafel/ios/prsh.h"
+#include "wafel/hai.h"
 #include "mbr.h"
+#include "rednand_config.h"
 
 const char* MODULE_NAME = "SDUSB";
 
@@ -129,6 +132,28 @@ out_free:
     return ret;
 }
 
+void hai_write_file_patch(trampoline_t_state *s){
+    uint32_t *buffer = (uint32_t*)s->r[1];
+    debug_printf("HAI WRITE COMPANION\n");
+    if(hai_getdev() == DEVTYPE_USB){
+        hai_companion_add_offset(buffer, sdusb_offset);
+    }
+}
+
+void hai_ios_patches(trampoline_t_state *s){
+    if(hai_getdev() == DEVTYPE_USB)
+        hai_redirect_mlc2sd();
+}
+
+void apply_hai_patches(void){
+    hai_apply_getdev_patch();
+    trampoline_t_hook_before(0x050078AE, hai_write_file_patch);
+    //apply patches to HAI IOS just before it gets launched
+    trampoline_t_hook_before(0x0500881e, hai_ios_patches);
+    //force device in hai parm to MLC
+    ASM_T_PATCH_K(0x05100198, "nop");
+}
+
 void hook_register_sd(trampoline_state *state){
     int *server_handle = (int*)state->r[0] -3;
     debug_printf("%s: org server_handle: %p\n", MODULE_NAME, server_handle);
@@ -149,6 +174,19 @@ void crypto_hook(trampoline_state* state){
         // tells crypto to not do crypto (depends on stroopwafel patch)
         state->r[0] = 0xDEADBEEF;
     }
+    //debug_printf("crypto_hook: r0: %p, r1: %p, r2: %p, r3: %p, r4: %p, r5: %p, r6: %p, r7: %p, r8: %p, r9: %p, r10: %p, r11: %p, r12: %p, lr: %p\n",
+    //        state->r[0],state->r[1],state->r[2],state->r[3],state->r[4],state->r[5],state->r[6],state->r[7],state->r[8],state->r[9],state->r[10],state->r[11], state->r[12], state->lr);
+}
+
+// int crypto_disable_hook(int r0, int r1, u32 r2, u32 r3, int (*dencrypt_async)(int, int, u32, u32, int, u32, int, int, int), int arg5, u32 arg6, int arg7, int arg8, int arg9){
+
+// }
+
+void test_hook(trampoline_state* state){
+    int *data = (int*)state->r[2];
+    for(int i=0; i<16; i++){
+        debug_printf("%X: %08X\n", i, data[i]);
+    }
 }
 
 // This fn runs before everything else in kernel mode.
@@ -163,9 +201,22 @@ void kern_main()
 
     debug_printf("init_linking symbol at: %08x\n", wafel_find_symbol("init_linking"));
 
+    rednand_config *rednand_conf;
+    size_t rednand_conf_size;
+    if(!prsh_get_entry("rednand", (void**)&rednand_conf, &rednand_conf_size)){
+        if(rednand_conf_size<sizeof(rednand_config_v1) || rednand_conf->mlc.lba_length){
+            debug_printf("%s: detected MLC redurection, %s will be disabled\n", MODULE_NAME, MODULE_NAME);
+            return;
+        }
+    }
+
     trampoline_hook_before(0x107bd9a4, hook_register_sd);
     trampoline_hook_before(0x10740f48, crypto_hook); // hook decrypt call
     trampoline_hook_before(0x10740fe8, crypto_hook); // hook encrypt call
+
+    debug_printf("%s: patches applied\n", MODULE_NAME);
+
+    //trampoline_hook_before(0x10740f2c, test_hook);
 }
 
 // This fn runs before MCP's main thread, and can be used
